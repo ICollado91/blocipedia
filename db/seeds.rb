571d225ad62bce0c529cfd003d4e652d9b506cca
Ivan <collado.ip@gmail.com>
2017-04-26 12:28:29 +0000

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+arr = []
+50.times do
+    User.create!(
+        email: Faker::Internet.unique.email,
+        password: Faker::Address.city
+    )
+    arr.push(User.last)
+end
+
+100.times do
+    Wiki.create!(
+        title: Faker::Hipster.unique.sentence,
+        body: Faker::Hipster.paragraph,
+        private: false,
+        user_id: arr.sample.id
+    )
+end
+
+puts "#{User.count} users were created!"
+puts "#{Wiki.count} wikis were created!"
